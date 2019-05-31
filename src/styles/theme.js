8a941f3676de64ffc690877a38518815839668db
DimaKoval <unig.dk@gmail.com>
2019-05-31 15:32:52 +0300

    feat: Footer

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -5,6 +5,55 @@ export default {
       brand: '#5d1943',
       lightGray: '#e9e9e9',
       subText: '#95989a',
+
+      control: {
+        dark: 'white',
+        light: 'brand',
+      },
+    },
+  },
+
+  button: {
+    border: {
+      radius: '3px',
+      width: '1px',
+    },
+    padding: {
+      vertical: '5px 20px 7px 20px',
+      horizontal: '',
+    },
+  },
+
+  text: {
+    xsmall: {
+      size: '12px',
+      height: '16px',
+      maxWidth: '288px',
+    },
+    small: {
+      size: '15px',
+      height: 1.2,
+      maxWidth: '336px',
+    },
+    medium: {
+      size: '20px',
+      height: '24px',
+      maxWidth: '432px',
+    },
+    large: {
+      size: '26px',
+      height: '30px',
+      maxWidth: '528px',
+    },
+    xlarge: {
+      size: '30px',
+      height: '36px',
+      maxWidth: '624px',
+    },
+    xxlarge: {
+      size: '34px',
+      height: '40px',
+      maxWidth: '816px',
     },
   },
 };
