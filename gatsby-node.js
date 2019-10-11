ba5519549ee4e07c73c898203b67eb801a45c12b
Simen A. W. Olsen <cobraz@cobraz.no>
2019-10-11 18:49:08 +0200

    chore: remove createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -52,99 +52,99 @@ fragment HighlightFragment on Prismic${type}BodyHighlight {
 }
 `;
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-  /**
-   * Static Pages
-   */
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   /**
+//    * Static Pages
+//    */
 
-  const pages = await graphql(`
-    {
-      allPrismicPage {
-        edges {
-          node {
-            id
-            slugs
-            type
-            uid
-            href
-            data {
-              title {
-                text
-              }
-              body {
-                ...GalleryFragment
-                ...TextFragment
-                ...HighlightFragment
-              }
-            }
-          }
-        }
-      }
-    }
-    ${GalleryFragment('Page')}
-    ${TextFragment('Page')}
-    ${HighlightFragment('Page')}
-  `);
+//   const pages = await graphql(`
+//     {
+//       allPrismicPage {
+//         edges {
+//           node {
+//             id
+//             slugs
+//             type
+//             uid
+//             href
+//             data {
+//               title {
+//                 text
+//               }
+//               body {
+//                 ...GalleryFragment
+//                 ...TextFragment
+//                 ...HighlightFragment
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//     ${GalleryFragment('Page')}
+//     ${TextFragment('Page')}
+//     ${HighlightFragment('Page')}
+//   `);
 
-  const templatePage = path.resolve('src/pages/page.jsx');
+//   const templatePage = path.resolve('src/pages/page.jsx');
 
-  pages.data.allPrismicPage.edges.forEach((edge) => {
-    createPage({
-      path: `/${edge.node.uid}`,
-      component: templatePage,
-      context: {
-        uid: edge.node.uid,
-        data: edge.node.data,
-      },
-    });
-  });
+//   pages.data.allPrismicPage.edges.forEach((edge) => {
+//     createPage({
+//       path: `/${edge.node.uid}`,
+//       component: templatePage,
+//       context: {
+//         uid: edge.node.uid,
+//         data: edge.node.data,
+//       },
+//     });
+//   });
 
-  /**
-   * Posts
-   */
+//   /**
+//    * Posts
+//    */
 
-  const posts = await graphql(`
-    {
-      allPrismicPost {
-        edges {
-          node {
-            id
-            slugs
-            type
-            uid
-            href
-            data {
-              title {
-                text
-              }
-              body {
-                ...GalleryFragment
-                ...TextFragment
-                ...HighlightFragment
-              }
-            }
-          }
-        }
-      }
-    }
-    ${GalleryFragment('Post')}
-    ${TextFragment('Post')}
-    ${HighlightFragment('Post')}
-  `);
+//   const posts = await graphql(`
+//     {
+//       allPrismicPost {
+//         edges {
+//           node {
+//             id
+//             slugs
+//             type
+//             uid
+//             href
+//             data {
+//               title {
+//                 text
+//               }
+//               body {
+//                 ...GalleryFragment
+//                 ...TextFragment
+//                 ...HighlightFragment
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//     ${GalleryFragment('Post')}
+//     ${TextFragment('Post')}
+//     ${HighlightFragment('Post')}
+//   `);
 
-  console.log(posts);
+//   console.log(posts);
 
-  const templatePost = path.resolve('src/pages/page.jsx');
+//   const templatePost = path.resolve('src/pages/page.jsx');
 
-  posts.data.allPrismicPost.edges.forEach((edge) => {
-    createPage({
-      path: `/blog/${edge.node.uid}`,
-      component: templatePost,
-      context: {
-        uid: edge.node.uid,
-        data: edge.node.data,
-      },
-    });
-  });
-};
+//   posts.data.allPrismicPost.edges.forEach((edge) => {
+//     createPage({
+//       path: `/blog/${edge.node.uid}`,
+//       component: templatePost,
+//       context: {
+//         uid: edge.node.uid,
+//         data: edge.node.data,
+//       },
+//     });
+//   });
+// };
