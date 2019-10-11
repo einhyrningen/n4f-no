0d018631fee685ba876293653226910452743285
Simen A. W. Olsen <cobraz@cobraz.no>
2019-10-11 18:55:21 +0200

    chore: removing pageContext vars

--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -8,8 +8,8 @@ const Page = ({ pageContext }) => {
   return (
     <Grommet theme={theme}>
       <N4FLayout>
-        <Heading>{pageContext.data.title.text}</Heading>
-        <Slice body={pageContext.data.body} />
+        {/* <Heading>{pageContext.data.title.text}</Heading> */}
+        {/* <Slice body={pageContext.data.body} /> */}
       </N4FLayout>
     </Grommet>
   );
